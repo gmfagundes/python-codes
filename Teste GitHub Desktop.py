c211a1aa56d7e1c144b7a2d9867685a1f2f47785
Gabriel Fagundes <gmfagundes@gmail.com>
2018-01-11 11:12:17 +0900

    add first commit

---
+++ Teste GitHub Desktop.py
@@ -0,0 +1,11 @@
+        Python 2.7.13 |Anaconda 4.4.0 (64-bit)| (default, May 11 2017, 13:17:26) [MSC v.1500 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> print("hello world")
+hello world
+	
+>>> def asd(x):
+	return x + 1
+
+>>> print asd(1)
+2
+>>> 
